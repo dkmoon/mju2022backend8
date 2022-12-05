68f8adf3cf1adb2075591bc9a75ed60ab3a1179f
DK Moon <dkmoon@gmail.com>
2022-12-05 23:09:27 +0900

    myserver solution 추가

---
+++ myserver/myserver/common.cpp
@@ -0,0 +1,5 @@
+#include "common.h"
+
+double makeDouble(int input) {
+	return input * 1.0;
+}
---
+++ myserver/myserver/myserver.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+#include "common.h"
+
+using namespace std;
+
+
+int main() {
+	cout.setf(ios::fixed);
+	cout << makeDouble(10) << endl;
+	cout << makeDouble(20) << endl;
+}
